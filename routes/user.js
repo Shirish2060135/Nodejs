32a2a4902a3a0b1f843d998b44bf4b9fda96e6f9
Shirish <np03cs4s210195@heraldcollege.edu.np>
2023-05-31 01:46:48 +0545

    node

---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const router = express.Router();
+//login page
+router.get('/login',(req,res)=> res.render('login'));
+//register page
+router.get('/register',(req,res)=> res.render('Register'));
+
+module.exports = router;
